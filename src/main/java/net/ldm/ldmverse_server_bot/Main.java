a53ccf1db84666e7796e4b2129c2e59622bf50f0
Logan Dhillon <logandhi64@gmail.com>
2023-02-21 15:30:17 -0500

    Attempted to read from `resources`

--- src/main/java/net/ldm/ldmverse_server_bot/Main.java
+++ src/main/java/net/ldm/ldmverse_server_bot/Main.java
@@ -3,11 +3,14 @@ package net.ldm.ldmverse_server_bot;
 import com.google.gson.GsonBuilder;
 import net.ldm.ldmverse_server_bot.file.FileUtils;
 
+import java.util.Objects;
+
 public class Main {
     public static final String TOKEN = new GsonBuilder().create().fromJson("", String.class);
 
     public static void main(String[] args) {
-        System.out.println(FileUtils.readFile(Main.class.getResource("bot.json")));
+        //System.out.println(FileUtils.readFile("classpath:bot.json"));
         //System.out.println(Main.class.getResourceAsStream("bot.json"));
+        System.out.println(FileUtils.readFile("bot.json"));
     }
 }
--- src/main/java/net/ldm/ldmverse_server_bot/file/FileUtils.java
+++ src/main/java/net/ldm/ldmverse_server_bot/file/FileUtils.java
@@ -1,14 +1,19 @@
 package net.ldm.ldmverse_server_bot.file;
 
+import net.ldm.ldmverse_server_bot.Main;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class FileUtils {
-    public static String readFile(URL path) {
+    public static String readFileD(String path) {
         StringBuilder content = new StringBuilder();
-        try (Scanner scanner = new Scanner(new File(path.getFile()))) {
+        try (Scanner scanner = new Scanner(new File(path))) {
             while (scanner.hasNextLine()) {
                 content.append(scanner.nextLine());
             }
@@ -16,5 +21,16 @@ public class FileUtils {
             throw new RuntimeException(e);
         }
         return content.toString();
+    }
+
+    public static String readFile(String path) {
+        try (InputStream inputStream = Main.class.getResourceAsStream(path)) {
+            if (inputStream != null) {
+                return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "File not found.";
     }
 }
