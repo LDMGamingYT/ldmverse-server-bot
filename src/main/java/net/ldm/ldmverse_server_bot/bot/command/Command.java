5cc67a2a460ed271a7598fbea37d39cde8a1c08b
Logan Dhillon <logandhi64@gmail.com>
2023-02-24 00:25:42 -0500

    Created sendSimpleEmbed()

--- src/main/java/net/ldm/ldmverse_server_bot/bot/command/Command.java
+++ src/main/java/net/ldm/ldmverse_server_bot/bot/command/Command.java
@@ -1,7 +1,11 @@
 package net.ldm.ldmverse_server_bot.bot.command;
 
+import net.dv8tion.jda.api.entities.EmbedType;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.ldm.ldmverse_server_bot.registry.RegistryObject;
+
+import java.time.OffsetDateTime;
 
 public abstract class Command implements RegistryObject {
     private final String command;
@@ -14,5 +18,13 @@ public abstract class Command implements RegistryObject {
 
     public String getCommand() {
         return command;
+    }
+
+    protected void sendSimpleEmbed(SlashCommandInteractionEvent event, String title, String description, int color,
+                                   String thumbnailUrl, String author, String authorIconUrl, String footer) {
+        event.replyEmbeds(new MessageEmbed(null, title, description, EmbedType.RICH, OffsetDateTime.now(), color,
+                new MessageEmbed.Thumbnail(thumbnailUrl, null, 100000, 100000), null,
+                new MessageEmbed.AuthorInfo(author, null, authorIconUrl, null), null,
+                new MessageEmbed.Footer(footer, null, null), null, null)).queue();
     }
 }
