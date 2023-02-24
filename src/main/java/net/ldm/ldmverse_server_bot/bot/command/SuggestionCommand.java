f96624bca32bac1cf32b20c6f467ffb31fdf11c3
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-02-24 07:35:33 -0500

    Gets resources from GitHub, no longer Imgur

--- src/main/java/net/ldm/ldmverse_server_bot/bot/command/SuggestionCommand.java
+++ src/main/java/net/ldm/ldmverse_server_bot/bot/command/SuggestionCommand.java
@@ -2,6 +2,7 @@ package net.ldm.ldmverse_server_bot.bot.command;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.ldm.ldmverse_server_bot.bot.init.BotHandler;
+import net.ldm.ldmverse_server_bot.resource.RemoteResourceGetter;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 
@@ -22,26 +23,25 @@ public class SuggestionCommand extends Command {
             return;
         }
 
-        // TODO: 2023-02-24 Move all assets (images) to GitHub, instead of Imgur
         switch (status) {
             case "approve" -> sendSimpleEmbed(event, "This suggestion has been approved!",
                     "Thanks for sharing your opinion! Your suggestion has been queued up for development!",
-                    0x0099e5, "https://i.imgur.com/w2vqd4Y.png",
+                    0x0099e5, RemoteResourceGetter.getPathToResource("images/suggestion_approved.png"),
                     "Approved by @"+event.getUser().getName(), event.getUser().getAvatarUrl(),
                     "Suggestion approved as of");
             case "deny" -> sendSimpleEmbed(event, "This suggestion has been denied.",
                     "Thanks for sharing your opinion! Unfortunately, this suggestion has not been approved, and will not be added.",
-                    0xCE1842, "https://i.imgur.com/kFYwXwL.png",
+                    0xCE1842, RemoteResourceGetter.getPathToResource("images/suggestion_denied.png"),
                     "Denied by @"+event.getUser().getName(), event.getUser().getAvatarUrl(),
                     "Suggestion denied as of");
             case "implement" -> sendSimpleEmbed(event, "This suggestion has been implemented!",
             "Thanks for sharing your opinion! Your suggestion has made it to the finish line, and will be released in the next update!",
-                    0x78b354, "https://i.imgur.com/F5N3NXt.png",
+                    0x78b354, RemoteResourceGetter.getPathToResource("images/suggestion_implemented.png"),
                     "Marked as implemented by @"+event.getUser().getName(), event.getUser().getAvatarUrl(),
                     "Suggestion implemented as of");
             case "consider" -> sendSimpleEmbed(event, "This suggestion is being considered!",
                     "Thanks for sharing your opinion! Your suggestion is being considered.",
-                    0xFFCC4B, "https://i.imgur.com/fnTwuss.png",
+                    0xFFCC4B, RemoteResourceGetter.getPathToResource("images/suggestion_considered.png"),
                     "Marked as considered by @"+event.getUser().getName(), event.getUser().getAvatarUrl(),
                     "Suggestion marked as considered as of");
             default -> {
