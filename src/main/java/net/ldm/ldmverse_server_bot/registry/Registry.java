c7088b1231709e20da822ae3040247601f279b82
Logan Dhillon <logandhi64@gmail.com>
2023-02-23 23:27:56 -0500

    Registry map is managed privately

--- src/main/java/net/ldm/ldmverse_server_bot/registry/Registry.java
+++ src/main/java/net/ldm/ldmverse_server_bot/registry/Registry.java
@@ -4,9 +4,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Registry {
-    public static final HashMap<Registries, ArrayList<RegistryObject>> REGISTRY_MAP = new HashMap<>();
+    private static final HashMap<Registries, ArrayList<RegistryObject>> REGISTRY_MAP = new HashMap<>();
 
-    public static RegistryObject register(Registries registry, RegistryObject item) {
+    public static boolean register(Registries registry, RegistryObject item) {
         return item.register(registry);
+    }
+
+    public static boolean isTypeEmpty(Registries registryType) {
+        return REGISTRY_MAP.isEmpty() || !REGISTRY_MAP.containsKey(registryType) || REGISTRY_MAP.get(registryType).isEmpty();
+    }
+
+    public static boolean add(Registries registryType, RegistryObject item) {
+        REGISTRY_MAP.putIfAbsent(registryType, new ArrayList<>());
+        return REGISTRY_MAP.get(registryType).add(item);
+    }
+
+    public static ArrayList<RegistryObject> getRegisteredFrom(Registries type) {
+        return REGISTRY_MAP.get(type);
     }
 }
--- src/main/java/net/ldm/ldmverse_server_bot/registry/RegistryObject.java
+++ src/main/java/net/ldm/ldmverse_server_bot/registry/RegistryObject.java
@@ -1,14 +1,11 @@
 package net.ldm.ldmverse_server_bot.registry;
 
-import java.util.ArrayList;
-import java.util.Objects;
-
 public interface RegistryObject {
-    default RegistryObject register(Registries registryType) {
-        if (!Registry.REGISTRY_MAP.containsKey(registryType))
-            Objects.requireNonNull(Registry.REGISTRY_MAP.put(registryType, new ArrayList<>())).add(this);
-        else
-            Registry.REGISTRY_MAP.get(registryType).add(this);
-        return this;
+    /**
+     * This method should only be used by {@link Registry}.<br>
+     * To register a {@link RegistryObject}, use {@link Registry#register}
+     */
+    default boolean register(Registries registryType) {
+        return Registry.add(registryType, this);
     }
 }
