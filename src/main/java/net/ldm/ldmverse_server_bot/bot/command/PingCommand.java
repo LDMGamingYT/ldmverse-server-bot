e22ba5cabb9ff6c3dfb3320b6e5978f21c3419f0
Logan Dhillon <logandhi64@gmail.com>
2023-02-24 00:24:52 -0500

    Simplified constructor

--- src/main/java/net/ldm/ldmverse_server_bot/bot/command/PingCommand.java
+++ src/main/java/net/ldm/ldmverse_server_bot/bot/command/PingCommand.java
@@ -3,8 +3,8 @@ package net.ldm.ldmverse_server_bot.bot.command;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class PingCommand extends Command {
-    public PingCommand(String command, String description) {
-        super(command);
+    public PingCommand() {
+        super("ping");
     }
 
     @Override
