8c1ef5c726de725ca5b2c9d390291e40bd3848d9
Logan Dhillon <logandhi64@gmail.com>
2023-02-24 00:24:39 -0500

    Registered /suggestion

--- src/main/java/net/ldm/ldmverse_server_bot/bot/init/BotRegistry.java
+++ src/main/java/net/ldm/ldmverse_server_bot/bot/init/BotRegistry.java
@@ -1,6 +1,7 @@
 package net.ldm.ldmverse_server_bot.bot.init;
 
 import net.ldm.ldmverse_server_bot.bot.command.PingCommand;
+import net.ldm.ldmverse_server_bot.bot.command.SuggestionCommand;
 import net.ldm.ldmverse_server_bot.registry.Registries;
 import net.ldm.ldmverse_server_bot.registry.Registry;
 import org.apache.logging.log4j.Logger;
@@ -10,6 +11,7 @@ public class BotRegistry {
     private static final Logger LOG = LoggerContext.getContext().getLogger(BotRegistry.class);
     public void initialize() {
         LOG.info("Registering items");
-        Registry.register(Registries.COMMAND, new PingCommand("ping", "Ping? Pong!"));
+        Registry.register(Registries.COMMAND, new PingCommand());
+        Registry.register(Registries.COMMAND, new SuggestionCommand());
     }
 }
