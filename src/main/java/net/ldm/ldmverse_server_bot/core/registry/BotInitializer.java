44fdb36635372785ef69403c88dba083b6d76fab
LDM <68861138+LDMGamingYT@users.noreply.github.com>
2023-02-24 22:53:28 -0500

    Removed useless method

--- src/main/java/net/ldm/ldmverse_server_bot/core/registry/BotInitializer.java
+++ src/main/java/net/ldm/ldmverse_server_bot/core/registry/BotInitializer.java
@@ -1,12 +1,7 @@
 package net.ldm.ldmverse_server_bot.core.registry;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.ldm.ldmverse_server_bot.bot.command.Command;
 import net.ldm.ldmverse_server_bot.bot.init.BotHandler;
@@ -33,37 +28,13 @@ public class BotInitializer {
                 return;
             }
 
-            SlashCommandData commmand = Commands.slash(registerCommand.getCommand(), registerCommand.getDescription())
+            Commands.slash(registerCommand.getCommand(), registerCommand.getDescription())
                     .setDefaultPermissions(DefaultMemberPermissions.enabledFor(registerCommand.getPermissions()))
                     .setGuildOnly(registerCommand.isGuildOnly()).addOptions(registerCommand.getOptions());
-
-            /*if (registerCommand.getOptions().length == 0) return;
-            for (OptionData option: registerCommand.getOptions()) {
-                commmand = commmand.addOptions(new OptionData(option.getType(), option.getName(), option.getDescription(),
-                        option.isRequired()).addChoices());
-            }*/
 
             commandsRegistered++;
         }
         action.queue();
         LOG.info("Registered {} commands", commandsRegistered);
-    }
-
-    private static void registerCommands(JDA bot) {
-        bot.updateCommands().addCommands(
-                Commands.slash("ping", "Ping? Pong!"),
-                Commands.slash("suggestion", "Set the status of a suggestion.")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR))
-                        .setGuildOnly(true)
-                        .addOptions(new OptionData(OptionType.STRING, "status", "The status of the suggestion", true)
-                                .addChoice("approve", "approve")
-                                .addChoice("deny", "deny")
-                                .addChoice("consider", "consider")
-                                .addChoice("implement", "implement")),
-                Commands.slash("set-level", "Sets a user's level")
-                        .addOption(OptionType.INTEGER, "level", "The new level", true)
-                // TODO: 2023-02-23 Add commands from 'Sapphire' bot
-        ).queue();
-        LOG.info("Registered {} commands", bot.retrieveCommands().complete().size());
     }
 }
